d971aaca96fdfadc90587b2ad6640851cd324460
Angel Emilio Lopez Fernandez <Appjak34@MacBook-Pro-de-Angel.local>
2017-05-01 00:14:09 -0500

    Se puso el imput para el numero de platillos a pedir

--- resources/views/templates/menu.blade.php
+++ resources/views/templates/menu.blade.php
@@ -6,6 +6,7 @@
                 <div class="col-sm-6">
                     <h4 class="mg-md  mg-sm-sm text-left-sm titulos tc-dark-red">{{$platillo->nombrePlatillo}}</h4>
                     <p>{{$platillo->descripcion}}</p>
+                    <div class="form-group"><input class="form-control" type="number" placeholder="# de Platillos a pedir" id="input_1517" /></div>
                 </div>
                 <div class="col-sm-6"><img src="{{$platillo->imagen}}" class="img-rd-lg mg-sm img-responsive center-block" height="166" width="238" />
                     <div>
